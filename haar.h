f0500055a324fa529cbf5803ac1250ff69be855d
Ahmet Inan <xdsopl@gmail.com>
2021-04-24 10:31:48 +0200

    stepsize of two makes it much more readable

--- haar.h
+++ haar.h
@@ -26,38 +26,38 @@ void ihaar(float *out, float *in, int N, int S)
 
 void haar2d(float *out, float *in, int N, int S)
 {
-	for (int l = N / 2; l > 0; l /= 2) {
-		for (int j = 0; j < l; ++j) {
-			for (int i = 0; i < l; ++i) {
-				float a = in[(N*(2*j+0)+2*i+0)*S], b = in[(N*(2*j+0)+2*i+1)*S];
-				float c = in[(N*(2*j+1)+2*i+0)*S], d = in[(N*(2*j+1)+2*i+1)*S];
-				out[(N*(j+0)+i+0)*S] = (a + b + c + d) / 2.f;
-				out[(N*(j+0)+i+l)*S] = ((a - b) + (c - d)) / 2.f;
-				out[(N*(j+l)+i+0)*S] = ((a - c) + (b - d)) / 2.f;
-				out[(N*(j+l)+i+l)*S] = ((a - b) - (c - d)) / 2.f;
+	for (int l = N; l >= 2; l /= 2) {
+		for (int j = 0; j < l; j += 2) {
+			for (int i = 0; i < l; i += 2) {
+				float a = in[(N*(j+0)+i+0)*S], b = in[(N*(j+0)+i+1)*S];
+				float c = in[(N*(j+1)+i+0)*S], d = in[(N*(j+1)+i+1)*S];
+				out[(N*(j+0)+i+0)/2*S] = (a + b + c + d) / 2.f;
+				out[(N*(j+0)+i+l)/2*S] = ((a - b) + (c - d)) / 2.f;
+				out[(N*(j+l)+i+0)/2*S] = ((a - c) + (b - d)) / 2.f;
+				out[(N*(j+l)+i+l)/2*S] = ((a - b) - (c - d)) / 2.f;
 			}
 		}
-		for (int j = 0; j < l; ++j)
-			for (int i = 0; i < l; ++i)
+		for (int j = 0; j < l / 2; ++j)
+			for (int i = 0; i < l / 2; ++i)
 				in[(N*j+i)*S] = out[(N*j+i)*S];
 	}
 }
 
 void ihaar2d(float *out, float *in, int N, int S)
 {
-	for (int l = 1; l < N; l *= 2) {
-		for (int j = 0; j < l; ++j) {
-			for (int i = 0; i < l; ++i) {
-				float a = in[(N*(j+0)+i+0)*S], b = in[(N*(j+0)+i+l)*S];
-				float c = in[(N*(j+l)+i+0)*S], d = in[(N*(j+l)+i+l)*S];
-				out[(N*(2*j+0)+2*i+0)*S] = (a + b + c + d) / 2.f;
-				out[(N*(2*j+0)+2*i+1)*S] = ((a - b) + (c - d)) / 2.f;
-				out[(N*(2*j+1)+2*i+0)*S] = ((a - c) + (b - d)) / 2.f;
-				out[(N*(2*j+1)+2*i+1)*S] = ((a - b) - (c - d)) / 2.f;
+	for (int l = 2; l <= N; l *= 2) {
+		for (int j = 0; j < l; j += 2) {
+			for (int i = 0; i < l; i += 2) {
+				float a = in[(N*(j+0)+i+0)/2*S], b = in[(N*(j+0)+i+l)/2*S];
+				float c = in[(N*(j+l)+i+0)/2*S], d = in[(N*(j+l)+i+l)/2*S];
+				out[(N*(j+0)+i+0)*S] = (a + b + c + d) / 2.f;
+				out[(N*(j+0)+i+1)*S] = ((a - b) + (c - d)) / 2.f;
+				out[(N*(j+1)+i+0)*S] = ((a - c) + (b - d)) / 2.f;
+				out[(N*(j+1)+i+1)*S] = ((a - b) - (c - d)) / 2.f;
 			}
 		}
-		for (int j = 0; j < 2 * l; ++j)
-			for (int i = 0; i < 2 * l; ++i)
+		for (int j = 0; j < l; ++j)
+			for (int i = 0; i < l; ++i)
 				in[(N*j+i)*S] = out[(N*j+i)*S];
 	}
 }
