6187b71180a870d0beac3ae01aa4163b68c3e59b
Ahmet Inan <xdsopl@gmail.com>
2021-04-27 10:52:40 +0200

    made all steps look the same

--- cdf97.h
+++ cdf97.h
@@ -18,21 +18,21 @@ void cdf97(float *out, float *in, int N, int SO, int SI)
 		d = 0.4435068522f,
 		e = 1.149604398f;
 
-	for (int i = 0; i < N-2; i += 2)
-		in[(i+1)*SI] += a * (in[(i+0)*SI] + in[(i+2)*SI]);
+	for (int i = 1; i < N-1; i += 2)
+		in[i*SI] += a * (in[(i-1)*SI] + in[(i+1)*SI]);
 	in[(N-1)*SI] += a * (2.f * in[(N-2)*SI]);
 
 	in[0] += b * (2.f * in[SI]);
 	for (int i = 2; i < N; i += 2)
-		in[(i+0)*SI] += b * (in[(i+1)*SI] + in[(i-1)*SI]);
+		in[i*SI] += b * (in[(i-1)*SI] + in[(i+1)*SI]);
 
-	for (int i = 0; i < N-2; i += 2)
-		in[(i+1)*SI] += c * (in[(i+0)*SI] + in[(i+2)*SI]);
+	for (int i = 1; i < N-1; i += 2)
+		in[i*SI] += c * (in[(i-1)*SI] + in[(i+1)*SI]);
 	in[(N-1)*SI] += c * (2.f * in[(N-2)*SI]);
 
 	in[0] += d * (2.f * in[SI]);
 	for (int i = 2; i < N; i += 2)
-		in[(i+0)*SI] += d * (in[(i+1)*SI] + in[(i-1)*SI]);
+		in[i*SI] += d * (in[(i-1)*SI] + in[(i+1)*SI]);
 
 	for (int i = 0; i < N; i += 2) {
 		out[(i+0)/2*SO] = in[(i+0)*SI] * e;
@@ -54,18 +54,18 @@ void icdf97(float *out, float *in, int N, int SO, int SI)
 	}
 	out[0] -= d * (2.f * out[SO]);
 	for (int i = 2; i < N; i += 2)
-		out[(i+0)*SO] -= d * (out[(i+1)*SO] + out[(i-1)*SO]);
+		out[i*SO] -= d * (out[(i-1)*SO] + out[(i+1)*SO]);
 
-	for (int i = 0; i < N-2; i += 2)
-		out[(i+1)*SO] -= c * (out[(i+0)*SO] + out[(i+2)*SO]);
+	for (int i = 1; i < N-1; i += 2)
+		out[i*SO] -= c * (out[(i-1)*SO] + out[(i+1)*SO]);
 	out[(N-1)*SO] -= c * (2.f * out[(N-2)*SO]);
 
 	out[0] -= b * (2.f * out[SO]);
 	for (int i = 2; i < N; i += 2)
-		out[(i+0)*SO] -= b * (out[(i+1)*SO] + out[(i-1)*SO]);
+		out[i*SO] -= b * (out[(i-1)*SO] + out[(i+1)*SO]);
 
-	for (int i = 0; i < N-2; i += 2)
-		out[(i+1)*SO] -= a * (out[(i+0)*SO] + out[(i+2)*SO]);
+	for (int i = 1; i < N-1; i += 2)
+		out[i*SO] -= a * (out[(i-1)*SO] + out[(i+1)*SO]);
 	out[(N-1)*SO] -= a * (2.f * out[(N-2)*SO]);
 }
 
