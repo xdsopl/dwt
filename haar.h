eac2295024bf0ef6acc14641d38e2fede8e05486
Ahmet Inan <xdsopl@gmail.com>
2021-05-28 22:49:23 +0200

    divide at least the sum by two

--- haar.h
+++ haar.h
@@ -1,12 +1,32 @@
 /*
 Haar wavelet
 
-Copyright 2014 Ahmet Inan <xdsopl@gmail.com>
+Copyright 2021 Ahmet Inan <xdsopl@gmail.com>
 */
 
 #pragma once
 
-#include <math.h>
+int half(int x)
+{
+	int bias = 1 << (sizeof(int) * 8 - 2);
+	return (x + bias) / 2 - bias / 2;
+}
+
+void haar(int *out, int *in, int N, int SO, int SI)
+{
+	for (int i = 0; i < N; i += 2) {
+		out[(i+0)/2*SO] = half(in[(i+0)*SI] + in[(i+1)*SI]);
+		out[(i+N)/2*SO] = in[(i+0)*SI] - in[(i+1)*SI];
+	}
+}
+
+void ihaar(int *out, int *in, int N, int SO, int SI)
+{
+	for (int i = 0; i < N; i += 2) {
+		out[(i+0)*SO] = in[(i+0)/2*SI] + half(in[(i+N)/2*SI] + 1);
+		out[(i+1)*SO] = in[(i+0)/2*SI] - half(in[(i+N)/2*SI]);
+	}
+}
 
 void haar2d(int *out, int *in, int N0, int N, int SO, int SI)
 {
@@ -15,9 +35,9 @@ void haar2d(int *out, int *in, int N0, int N, int SO, int SI)
 			for (int i = 0; i < l; i += 2) {
 				int a = in[(N*(j+0)+i+0)*SI], b = in[(N*(j+0)+i+1)*SI];
 				int c = in[(N*(j+1)+i+0)*SI], d = in[(N*(j+1)+i+1)*SI];
-				out[(N*(j+0)+i+0)/2*SO] = a + b + c + d;
-				out[(N*(j+0)+i+l)/2*SO] = (a - b) + (c - d);
-				out[(N*(j+l)+i+0)/2*SO] = (a - c) + (b - d);
+				out[(N*(j+0)+i+0)/2*SO] = half(half(a + b) + half(c + d));
+				out[(N*(j+0)+i+l)/2*SO] = half(a + b) - half(c + d);
+				out[(N*(j+l)+i+0)/2*SO] = half((a - b) + (c - d));
 				out[(N*(j+l)+i+l)/2*SO] = (a - b) - (c - d);
 			}
 		}
@@ -34,10 +54,10 @@ void ihaar2d(int *out, int *in, int N0, int N, int SO, int SI)
 			for (int i = 0; i < l; i += 2) {
 				int a = in[(N*(j+0)+i+0)/2*SI], b = in[(N*(j+0)+i+l)/2*SI];
 				int c = in[(N*(j+l)+i+0)/2*SI], d = in[(N*(j+l)+i+l)/2*SI];
-				out[(N*(j+0)+i+0)*SO] = (a + b + c + d) / 4;
-				out[(N*(j+0)+i+1)*SO] = ((a - b) + (c - d)) / 4;
-				out[(N*(j+1)+i+0)*SO] = ((a - c) + (b - d)) / 4;
-				out[(N*(j+1)+i+1)*SO] = ((a - b) - (c - d)) / 4;
+				out[(N*(j+0)+i+0)*SO] = a + half(b + 1) + half(c + half(d + 1) + 1);
+				out[(N*(j+0)+i+1)*SO] = a + half(b + 1) - half(c + half(d + 1));
+				out[(N*(j+1)+i+0)*SO] = a - half(b) + half(c - half(d) + 1);
+				out[(N*(j+1)+i+1)*SO] = a - half(b) - half(c - half(d));
 			}
 		}
 		for (int j = 0; j < l; ++j)
